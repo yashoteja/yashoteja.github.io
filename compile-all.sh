acf5b71c2ad338183561e09270a3071e83073cfd
Yashoteja Prabhu <t-yaprab@microsoft.com>
2019-10-16 10:24:46 +0530

    Updated details of WSDM'20 paper acceptance

--- compile-all.sh
+++ compile-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python jemdoc.py -c my.conf -o articles.html articles
-python jemdoc.py -c my.conf -o awards.html awards
-python jemdoc.py -c my.conf -o code.html code
-python jemdoc.py -c my.conf -o index.html index
-python jemdoc.py -c my.conf -o service.html service
+python2 jemdoc.py -c my.conf -o articles.html articles
+python2 jemdoc.py -c my.conf -o awards.html awards
+python2 jemdoc.py -c my.conf -o code.html code
+python2 jemdoc.py -c my.conf -o index.html index
+python2 jemdoc.py -c my.conf -o service.html service
