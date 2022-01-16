bcc8c72481832f2a8e10e654bf7c5ccc34d5ae2e
Yashoteja Prabhu <yashoteja.prabhu@gmail.com>
2022-01-16 14:48:22 +0530

    Latest update

--- compile-all.sh
+++ compile-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python2 jemdoc.py -c my.conf -o articles.html articles
-python2 jemdoc.py -c my.conf -o awards.html awards
-python2 jemdoc.py -c my.conf -o code.html code
-python2 jemdoc.py -c my.conf -o index.html index
-python2 jemdoc.py -c my.conf -o service.html service
+python2.7 jemdoc.py -c my.conf -o articles.html articles
+python2.7 jemdoc.py -c my.conf -o awards.html awards
+python2.7 jemdoc.py -c my.conf -o code.html code
+python2.7 jemdoc.py -c my.conf -o index.html index
+python2.7 jemdoc.py -c my.conf -o service.html service
